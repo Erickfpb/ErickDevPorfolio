a271bb765fd632865fc20d398fd455cd312bd751
erick <erick_fp@p@hotmail.com>
2021-12-16 21:05:35 -0600

    added js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var element = document.getElementById("myTopnav");
+    element.classList.toggle("responsive");
+}
+
+function myFunctionDark() {
+    var element = document.getElementById("dark");
+    element.classList.toggle("active");
+ }
+ 
